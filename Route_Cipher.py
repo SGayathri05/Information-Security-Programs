0777c3b0a5035ae2c4e5fd607bea8d9f88eaf052
S GAYATHRI IYER <81139359+SGayathri05@users.noreply.github.com>
2023-12-07 13:39:04 +0530

    Create Route_Cipher.py

---
+++ Route_Cipher.py
@@ -0,0 +1,34 @@
+def route_cipher(message, rows, cols):
+  message+= ' ' * (rows * cols- len(message))
+  grid = [[' ' for _ in range(cols)] for _ in range(rows)]
+
+  for r in range(rows):
+    for c in range(cols):
+      grid[r][c] = message[r*cols+c]
+
+  ciphertext = ''
+  for c in range(cols):
+    for r in range(rows):
+      ciphertext += grid[r][c]
+  
+  return ciphertext
+
+def route_cipher_decrypt(ciphertext, rows, cols):
+  grid = [[' ' for _ in range(cols)] for _ in range(rows)]
+
+  for r in range(rows):
+    for c in range(cols):
+      grid[r][c] = ''
+
+  i=0
+  for c in range(cols):
+    for r in range(rows):
+      grid[r][c] = ciphertext[i]
+      i = i+1
+
+  plaintext = ''
+  for r in range(rows):
+    for c in range(cols):
+      plaintext += grid[r][c]
+
+  return plaintext.strip()  
